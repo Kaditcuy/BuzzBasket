1a84c18ac90535dce180ddffbad49aa4a4eac004
Kaditcuy <philipukchimaobi@gmail.com>
2023-09-11 14:33:09 +0100

    Fixed lint issues

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-//Define the MongoDb connection URI using environmental variables
+// Define the MongoDb connection URI using environmental variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/buzzbasket-databse';
 
-
-//Create a Mongoose instance and connect to the database
+// Create a Mongoose instance and connect to the database
 mongoose.connect(mongoURI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
-	console.log('MongoDB connected');
+  console.log('MongoDB connected');
 }).catch((error) => {
-	console.error('MongoDB connection error', error);
+  console.error('MongoDB connection error', error);
 });
 
 module.exports = mongoose;
-
