becca7e1190bdf1837f3eb54becc4c54ba9dfed5
Kaditcuy <philipukchimaobi@gmail.com>
2023-09-11 14:31:20 +0100

    Mongoose instance connecting to the mongodb database

---
+++ utils/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//Define the MongoDb connection URI using environmental variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/buzzbasket-databse';
+
+
+//Create a Mongoose instance and connect to the database
+mongoose.connect(mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => {
+	console.log('MongoDB connected');
+}).catch((error) => {
+	console.error('MongoDB connection error', error);
+});
+
+module.exports = mongoose;
+
