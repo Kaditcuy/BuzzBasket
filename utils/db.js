d8ba4658cacd60803f9f2cb22397489a72f691bb
rotex5 <dantejake48@gmail.com>
2023-09-23 19:59:37 +0100

    shipping, product and user controllers

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 // Define the MongoDb connection URI using environmental variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/buzzbasket-databse';
-
 // Create a Mongoose instance and connect to the database
+mongoose.set('debug', true);
+
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
